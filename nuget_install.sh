8489fcc8dec42e27d6917a5d54274c93cc8418bd
Kazunori SAKAMOTO <exkazuu@gmail.com>
2013-09-13 18:38:21 +0900

    Downgrade NUnit.Runner for Mono.

--- nuget_install.sh
+++ nuget_install.sh
@@ -1,5 +1,5 @@
 #/bin/sh
-mono --runtime=v4.0.30319 .nuget/NuGet.exe install NUnit.Runners -Version 2.6.2 -o packages
+mono --runtime=v4.0.30319 .nuget/NuGet.exe install NUnit.Runners -Version 2.6.1 -o packages
 
 find . -type f -name packages.config | while read FILE
 do
