47c68621b7c0908805501fd547c0cc4c0679b730
Kazunori SAKAMOTO <exkazuu@gmail.com>
2014-03-25 16:24:30 +0900

    Improve Travis-CI settings.

--- .ci/nunit.sh
+++ .ci/nunit.sh
@@ -2,14 +2,15 @@
 
 mono --runtime=v4.0 .nuget/NuGet.exe install NUnit.Runners -Version 2.6.1 -o packages
 
-runTest(){
-    mono --runtime=v4.0 packages/NUnit.Runners.2.6.1/tools/nunit-console.exe -noxml -nodots -labels -stoponerror $@
-   if [ $? -ne 0 ]
-   then   
-     exit 1
-   fi
-}
+rm -R **/obj
 
-runTest $1
+for arg in $@; do
+	find . -name ${arg} | while read file; do
+		mono --runtime=v4.0 packages/NUnit.Runners.2.6.1/tools/nunit-console.exe -noxml -nodots -labels -stoponerror "${file}"
+		if [ $? -ne 0 ]; then
+			exit 1
+		fi
+	done
+done
 
 exit $?
