41528ef53fc178d1745de3dee52e33beeb907164
Sakamoto, Kazunori <exkazuu@gmail.com>
2016-12-18 18:51:01 +0900

    Add XML document for NuGet package. Bump up version to 1.3.3.22.

--- deploy.bat
+++ deploy.bat
@@ -8,7 +8,7 @@ FOR %%f IN (*.csproj) DO (
 	%nuget% pack %%f -Prop Configuration=Release
 )
 FOR %%f IN (*.nupkg) DO (
-	%nuget% push %%f
+	%nuget% push %%f -Source https://www.nuget.org/api/v2/package
 )
 del *.nupkg
 cd ..
